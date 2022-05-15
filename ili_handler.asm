f1331d8a3577196ea99f98471c484a138c73d9c2
ofernis <ofernis73@gmail.com>
2022-05-16 01:30:41 +0300

    first edit

--- ili_handler.asm
+++ ili_handler.asm
@@ -3,5 +3,83 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  ####### Some smart student's code here #######
+  pushq %rsi
+  pushq %rbp
+  pushq %rsp
+  pushq %rax
+  pushq %rbx
+  pushq %rcx
+  pushq %rdx
+  pushq %r8
+  pushq %r9
+  pushq %r10
+  pushq %r11
+  pushq %r12
+  pushq %r13
+  pushq %r14
+  pushq %r15
+
+  xorq %rax, %rax
+  xorq %rcx, %rcx
+  xorq %rdi, %rdi
+  movq 120(%rsp), %rcx
+  movq (%rcx), %rcx
+  cmpb $0x0f, %cl
+  jne one_byte_opcode
+
+two_byte_opcode:
+  movb %ch, %al
+  movq %rax, %rdi
+  call what_to_do
+  cmpq $0, %rax
+  je default_ili_handler
+  jmp modified_ili_handler 
+
+one_byte__opcode:
+  movb %cl, %al
+  movq %rax, %rdi
+  call what_to_do
+  cmpq $0, %rax
+  je default_ili_handler
+  jmp modified_ili_handler
+
+default_ili_handler:
+  popq %rsi
+  popq %rbp
+  popq %rsp
+  popq %rax
+  popq %rbx
+  popq %rcx
+  popq %rdx
+  popq %r8
+  popq %r9
+  popq %r10
+  popq %r11
+  popq %r12
+  popq %r13
+  popq %r14
+  popq %r15
+  jmp * old_ili_handler
+  jmp end
+
+modified_ili_handler:
+  movq %rax, %rdi
+  popq %rsi
+  popq %rbp
+  popq %rsp
+  popq %rax
+  popq %rbx
+  popq %rcx
+  popq %rdx
+  popq %r8
+  popq %r9
+  popq %r10
+  popq %r11
+  popq %r12
+  popq %r13
+  popq %r14
+  popq %r15
+  addq $2, (%rsp)
+
+end:
   iretq
--- ili_utils.c
+++ ili_utils.c
@@ -1,25 +1,31 @@
 #include <asm/desc.h>
 
 void my_store_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// </STUDENT FILL>
+    asm volatile("sidt %0;"
+                :"=m" (*idtr)
+                :
+                :
+                );
 }
 
 void my_load_idt(struct desc_ptr *idtr) {
-// <STUDENT FILL> - HINT: USE INLINE ASSEMBLY
-
-// <STUDENT FILL>
+    asm volatile("lidt %0;"
+                :
+                :"m" (*idtr)
+                :
+                );
 }
 
 void my_set_gate_offset(gate_desc *gate, unsigned long addr) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-// </STUDENT FILL>
+    gate->offset_low = addr;
+    gate->offset_middle = addr >> 16;
+    gate->offset_high = addr >> 32;
 }
 
 unsigned long my_get_gate_offset(gate_desc *gate) {
-// <STUDENT FILL> - HINT: NO NEED FOR INLINE ASSEMBLY
-
-// </STUDENT FILL>
+    unsigned long address = gate->offset_high;
+    address = address << 16;
+    address += gate->offest_middle;
+    address = address << 16;
+    return (address + gate->offset_low);
 }
